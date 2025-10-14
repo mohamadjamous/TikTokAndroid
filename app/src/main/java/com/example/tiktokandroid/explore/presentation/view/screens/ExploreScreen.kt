e5fd4c32f586e53a70cd6675471cb2b7e2b0e33d
Mohamad Jamous <mohamad.jamous@gmail.com>
2025-10-14 21:53:40 +0300

    feat: update project structure

--- app/src/main/java/com/example/tiktokandroid/explore/presentation/view/screens/ExploreScreen.kt
+++ app/src/main/java/com/example/tiktokandroid/explore/presentation/view/screens/ExploreScreen.kt
@@ -1,9 +1,12 @@
 package com.example.tiktokandroid.explore.presentation.view.screens
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun ExploreScreen(modifier: Modifier = Modifier) {
+
+    Text("ExploreScreen")
 
 }
