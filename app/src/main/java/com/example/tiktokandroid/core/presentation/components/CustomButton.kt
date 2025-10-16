952a56e158ef19d9fcaa2efa85fc7b9c0dfa9d3c
Mohamad Jamous <mohamad.e.jamous@gmail.com>
2025-10-16 11:01:24 +0300

    feat: add profile header view

---
+++ app/src/main/java/com/example/tiktokandroid/core/presentation/components/CustomButton.kt
@@ -0,0 +1,47 @@
+package com.example.tiktokandroid.core.presentation.components
+
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun CustomButton(
+    modifier: Modifier = Modifier,
+    text: String,
+    action: () -> Unit,
+    containerColor: Color = Color(0xFFE5E5EA),
+    contentColor: Color = Color.Black
+) {
+
+
+    Button(
+        modifier = modifier.height(35.dp),
+        shape = RoundedCornerShape(5.dp),
+        onClick = action,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = containerColor,
+            contentColor = contentColor
+        )
+    ) {
+        Text(
+            text = text,
+            fontWeight = FontWeight.Bold,
+            color = contentColor
+        )
+    }
+
+}
+
+@Preview
+@Composable
+private fun GrayCustomButtonPreview() {
+    CustomButton(text = "edit", action = {})
+}
--- app/src/main/java/com/example/tiktokandroid/profile/presentation/view/screens/ProfileHeaderView.kt
+++ app/src/main/java/com/example/tiktokandroid/profile/presentation/view/screens/ProfileHeaderView.kt
@@ -1,17 +1,22 @@
 package com.example.tiktokandroid.profile.presentation.view.screens
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tiktokandroid.core.presentation.components.CircularGlideImage
+import com.example.tiktokandroid.core.presentation.components.CustomButton
 
 @Composable
 fun ProfileHeaderView(
@@ -19,8 +24,9 @@ fun ProfileHeaderView(
     imageUrl: String = "",
     username: String = "",
     following: Int = 0,
-    follower: Int = 0,
-    likes: Int = 0) {
+    followers: Int = 0,
+    likes: Int = 0
+) {
 
     Column(
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -37,6 +43,83 @@ fun ProfileHeaderView(
         )
 
 
+        Box(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ) {
+
+            Row(
+                modifier = Modifier
+                    .padding(top = 20.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+
+                Column(
+                    modifier = Modifier.padding(horizontal = 30.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "$following",
+                        fontWeight = FontWeight.Bold,
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+
+                    Text(
+                        text = "following",
+                        color = Color.Gray,
+                        style = MaterialTheme.typography.bodySmall
+                    )
+                }
+
+                Column(
+                    modifier = Modifier.padding(horizontal = 30.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "$followers",
+                        fontWeight = FontWeight.Bold,
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+
+                    Text(
+                        text = "followers",
+                        color = Color.Gray,
+                        style = MaterialTheme.typography.bodySmall
+                    )
+                }
+                Column(
+                    modifier = Modifier.padding(horizontal = 30.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "$likes",
+                        fontWeight = FontWeight.Bold,
+                        style = MaterialTheme.typography.bodyLarge
+                    )
+
+                    Text(
+                        text = "likes",
+                        color = Color.Gray,
+                        style = MaterialTheme.typography.bodySmall
+                    )
+                }
+
+
+            }
+
+        }
+
+
+
+        CustomButton(
+            modifier = Modifier.fillMaxWidth().padding(all = 20.dp),
+            text = "Edit Profile",
+            action = {
+
+            }
+        )
+
+
     }
 
 
@@ -45,7 +128,7 @@ fun ProfileHeaderView(
 
 @Composable
 @Preview(showSystemUi = true)
-fun ProfileHeaderViewPreview(){
+fun ProfileHeaderViewPreview() {
     ProfileHeaderView(
         username = "username default",
         imageUrl = "https://letsenhance.io/static/73136da51c245e80edc6ccfe44888a99/396e9/MainBefore.jpg"
