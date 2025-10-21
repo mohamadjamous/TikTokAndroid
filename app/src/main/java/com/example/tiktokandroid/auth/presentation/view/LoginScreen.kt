0b0ef9cf30c3670708863559f49161d5f7f0a0b1
Mohamad Jamous <mohamad.e.jamous@gmail.com>
2025-10-21 18:35:19 +0300

    feat: add login screen

--- app/src/main/java/com/example/tiktokandroid/auth/presentation/view/LoginScreen.kt
+++ app/src/main/java/com/example/tiktokandroid/auth/presentation/view/LoginScreen.kt
@@ -2,26 +2,68 @@ package com.example.tiktokandroid.auth.presentation.view
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.tiktokandroid.core.presentation.components.LoginButton
 
 @Composable
-fun LoginScreen(modifier: Modifier = Modifier) {
+fun LoginScreen(
+    modifier: Modifier = Modifier,
+    onEmailUsernameClick: () -> Unit,
+    onGoogleClick: () -> Unit
+) {
 
     Column(
         modifier = modifier.fillMaxSize(),
         verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally) {
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
 
         Text(
             text = "Login in",
-            style = MaterialTheme.typography.labelLarge
+            fontWeight = FontWeight.Bold,
+            fontSize = 30.sp
         )
+
+        Spacer(modifier = Modifier.height(30.dp))
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            LoginButton(
+                modifier = Modifier.fillMaxWidth(),
+                text = "Use phone / email / username",
+                leadingIcon = Icons.Default.Person,
+                onClick = { onEmailUsernameClick() }
+            )
+
+            Spacer(modifier = Modifier.height(15.dp))
+
+            LoginButton(
+                modifier = Modifier.fillMaxWidth(),
+                text = "Continue with Google",
+                leadingIcon = Icons.Default.Person,
+                onClick = { onGoogleClick() }
+            )
+
+        }
     }
 
 }
--- app/src/main/java/com/example/tiktokandroid/core/presentation/components/LoginButton.kt
+++ app/src/main/java/com/example/tiktokandroid/core/presentation/components/LoginButton.kt
@@ -1,13 +1,30 @@
 package com.example.tiktokandroid.core.presentation.components
 
-import android.graphics.drawable.Icon
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun LoginButton(
@@ -19,8 +36,37 @@ fun LoginButton(
     containerColor: Color = Color.White
 ) {
 
+    Button(
+        modifier = modifier.height(45.dp),
+        shape = RoundedCornerShape(5.dp),
+        border = BorderStroke(width = 1.dp, color = Color.LightGray),
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(
+            containerColor = containerColor,
+            contentColor = contentColor
+        )
+    ) {
+        Box(
+            modifier = Modifier.fillMaxWidth(),
+            contentAlignment = Alignment.Center
+        ) {
 
-    
+            Icon(
+                imageVector = leadingIcon,
+                contentDescription = "leading icon",
+                modifier = Modifier
+                    .align(Alignment.CenterStart)
+                    .size(20.dp)
+            )
+
+            Text(
+                text = text,
+                fontWeight = FontWeight.Bold,
+                style = MaterialTheme.typography.labelLarge,
+                color = contentColor
+            )
+        }
+    }
 
 }
 
