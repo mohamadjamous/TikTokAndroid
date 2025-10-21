0c987a3d1e354c7ee43d39ee8de1bc3eb7a530db
Mohamad Jamous <mohamad.e.jamous@gmail.com>
2025-10-21 15:55:26 +0300

    feat: add auth setup

---
+++ app/src/main/java/com/example/tiktokandroid/auth/presentation/view/SignupScreen.kt
@@ -0,0 +1,10 @@
+package com.example.tiktokandroid.auth.presentation.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun SignupScreen(modifier: Modifier = Modifier) {
+
+}
--- app/src/main/java/com/example/tiktokandroid/profile/presentation/view/screens/ProfileScreen.kt
+++ app/src/main/java/com/example/tiktokandroid/profile/presentation/view/screens/ProfileScreen.kt
@@ -1,18 +1,25 @@
 package com.example.tiktokandroid.profile.presentation.view.screens
 
+import android.widget.ImageButton
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -24,7 +31,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tiktokandroid.core.presentation.model.Post
 import com.example.tiktokandroid.profile.presentation.components.PostRowItem
-import com.example.tiktokandroid.profile.presentation.components.ProfileGridView
 
 @Composable
 fun ProfileScreen(modifier: Modifier = Modifier) {
@@ -54,17 +60,37 @@ fun ProfileScreen(modifier: Modifier = Modifier) {
             .padding(bottom = 55.dp),
         contentPadding = PaddingValues(bottom = 50.dp)
     ) {
-
         item(span = { GridItemSpan(maxLineSpan) }) {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier.fillMaxWidth()
             ) {
-                Text(
-                    text = "Profile",
-                    style = MaterialTheme.typography.bodyLarge,
-                    fontWeight = FontWeight.Bold,
-                    modifier = Modifier.padding(top = 20.dp)
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 20.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Spacer(Modifier.weight(1.3f))
+
+                    Text(
+                        text = "Profile",
+                        style = MaterialTheme.typography.bodyLarge,
+                        fontWeight = FontWeight.Bold
+                    )
+
+                    Spacer(Modifier.weight(1f))
+
+                    IconButton(onClick = { }) {
+                        Icon(Icons.Filled.Menu, contentDescription = "More options")
+                    }
+
+                }
+
+                Divider(
+                    modifier = Modifier.padding(top = 5.dp)
                 )
 
                 ProfileHeaderView(
@@ -74,7 +100,6 @@ fun ProfileScreen(modifier: Modifier = Modifier) {
                 )
             }
         }
-
         items(posts.size) { index ->
             PostRowItem(
                 modifier = Modifier.padding(2.dp),
@@ -82,8 +107,8 @@ fun ProfileScreen(modifier: Modifier = Modifier) {
             )
         }
     }
-}
 
+}
 
 
 @Composable
